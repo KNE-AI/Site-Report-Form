498359ff5f15db61d42da05b4a9473f6a0baa329
KNE-AI <kne.ai.iraq@gmail.com>
2025-07-01 13:31:48 +0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -4,32 +4,37 @@ import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { Toaster } from 'sonner-native';
 import HomeScreen from "./screens/HomeScreen"
-  
+import SurveyScreen from "./screens/SurveyScreen"
+import SurveyListScreen from "./screens/SurveyListScreen"
+
 const Stack = createNativeStackNavigator();
-  
+
 function RootStack() {
   return (
     <Stack.Navigator screenOptions={{
-    headerShown: false
-  }}>
-    <Stack.Screen name="Home" component={HomeScreen} />
-  </Stack.Navigator>
+      headerShown: false
+    }}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Survey" component={SurveyScreen} />
+      <Stack.Screen name="SurveyList" component={SurveyListScreen} />
+    </Stack.Navigator>
   );
 }
-  
+
 export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
-    <Toaster />
+      <Toaster />
       <NavigationContainer>
         <RootStack />
       </NavigationContainer>
     </SafeAreaProvider>
   );
 }
-  
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    userSelect: "none"
   }
 });
